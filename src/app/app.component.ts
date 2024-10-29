6688fdb7f17ef3d1bbce0c2d24d7ebe46d3e8c5b
mithu0621 <mithunisk03@gmail.com>
2024-10-29 09:35:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
